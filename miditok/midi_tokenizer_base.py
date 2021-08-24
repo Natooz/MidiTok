4731d86a6d35aff6e43a7d30482c92eb275f659b
Megha Sharma <55681191+ms3744@users.noreply.github.com>
2021-08-24 23:55:14 +0400

    Update midi_tokenizer_base.py

--- miditok/midi_tokenizer_base.py
+++ miditok/midi_tokenizer_base.py
@@ -258,6 +258,8 @@ class MIDITokenizer:
 
             # Converting the MIDI to tokens and saving them as json
             tokens, track_info = self.midi_to_tokens(midi)
+            # Making a directory for the JSON file
+            PurePath(out_dir, midi_path).mkdir(parents=True, exist_ok=True)
             with open(PurePath(out_dir, midi_path).with_suffix(".json"), 'w') as outfile:
                 json.dump([tokens[0], track_info[0]], outfile)
 
