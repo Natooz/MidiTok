b1c46dac0b998e541ed76ee1695470ffa46bb9f6
Nathan Fradet <56734983+Natooz@users.noreply.github.com>
2024-04-23 18:52:51 +0200

    docs update

--- docs/conf.py
+++ docs/conf.py
@@ -49,4 +49,3 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "furo"
-html_static_path = ["_static"]
