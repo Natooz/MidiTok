a0fe721bedc1487df714e1eb9d4a0503f2db0d75
Nathan Fradet <56734983+Natooz@users.noreply.github.com>
2024-05-13 22:46:31 +0200

    adding todo + update readme

--- miditok/midi_tokenizer.py
+++ miditok/midi_tokenizer.py
@@ -2561,6 +2561,7 @@ class MusicTokenizer(ABC, HFHubMixin):
                 )
             tokenizer = _HFTokenizer(getattr(_tok_models, model)(**model_kwargs))
             if self.config.encode_ids_split != "no":
+                # TODO the special char ▁ (U+2581) shouldn't be used elsewhere in vocab
                 tokenizer.pre_tokenizer = _pre_tokenizers.Metaspace()
                 tokenizer.decoder = _decoders.Metaspace()
         else:
